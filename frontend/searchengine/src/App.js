6638557dc2106d79302924fb44c6c37ed0529e01
Tomasz Kajda <92893264+tkajda@users.noreply.github.com>
2022-05-18 00:03:07 +0200

    bases of server

--- frontend/searchengine/src/App.js
+++ frontend/searchengine/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import {useState} from 'react';
+import axios from 'axios'
+
 
 function App() {
+  const [data, setData] = useState('')
+  const [links, setLinks] = useState([]);
+
+  const fetchData = () => {
+    console.log(data)
+
+    axios.get('http://localhost:80',
+      {
+        params: {data}
+      }
+    )
+    .then(response => {
+      console.log(response)
+      setLinks(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wyszukiwarka</h1>
+      <h2>Wprowadz fraze</h2>
+      <input
+        type = "text"
+        value = {data}
+        required
+        onChange={(e) => setData(e.target.value)}/>
+      <button onClick={fetchData}>Szukaj</button>
+      <div>
+      Znaleziono:
+      {
+        links.map(
+          link => <div className="singleResult" key={link.id}>{link.url}</div>
+        )
+      }
+      </div>
+
+  
     </div>
   );
 }
